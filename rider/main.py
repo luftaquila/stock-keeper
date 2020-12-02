01790ee1dfa972769f09d5901c0b4bcc02e27f6d
LUFT-AQUILA <mail@luftaquila.io>
2020-12-02 22:54:15 +0900

    removed filtering delisted company

--- rider/main.py
+++ rider/main.py
@@ -20,40 +20,11 @@ for index, row in stock_code.iterrows():
     url = 'http://finance.naver.com/item/sise_day.nhn?code={code}'.format(code=row.code)     
     url = '{url}&page={page}'.format(url=url, page=page)
     df = df.append(pd.read_html(url, header=0)[0], ignore_index=True).dropna()    
-    
-    # filter delisted company
-    if page == 1 and (not len(df) or datetime.datetime.now().strftime('%Y.%m.%d') != df.head(1)['날짜'].values[0]):
-      delisted_flag = True
-      break
-      
-  if delisted_flag:
-    stock_code.drop(index, inplace=True) # drop delisted company
-  else: # else add mean volume
-    print(row.code, row.company, c)
-    df['거래량'] = df['거래량'].astype(int)
-    stock_code[index, 'volume'] = df['거래량'].mean()
+
+  print(row.code, row.company)
+  df['거래량'] = df['거래량'].astype(int)
+  stock_code[index, 'volume'] = df['거래량'].mean()
+  
   
 print(stock_code)
 sys.exit()
-# targeting company
-company='LG화학' 
-code = stock_code[stock_code.company == company].code.values[0].strip() ## strip() : 공백제거
-
-
-# crawl target company's date by date data
-df = pd.DataFrame()
-for page in range(1, 21):
-  url = 'http://finance.naver.com/item/sise_day.nhn?code={code}'.format(code=code)     
-  url = '{url}&page={page}'.format(url=url, page=page)
-  print('working on: ', url)
-  df = df.append(pd.read_html(url, header=0)[0], ignore_index=True)
-  
-    
-    
-df = df.dropna() # remove NaN rows
-df = df.rename(columns= {'날짜': 'date', '종가': 'close', '전일비': 'diff', '시가': 'open', '고가': 'high', '저가': 'low', '거래량': 'volume'}) 
-df[['close', 'diff', 'open', 'high', 'low', 'volume']] = df[['close', 'diff', 'open', 'high', 'low', 'volume']].astype(int) # change datatype to int
-df['date'] = pd.to_datetime(df['date']) # change date's datatype to date
-df = df.sort_values(by=['date'], ascending=True) 
-print(df) 
-
